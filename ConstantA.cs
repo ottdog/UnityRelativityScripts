d3a2f4753fbd9f79211d2c5bfd75e6f30949f351
ottdog <55850761+ottdog@users.noreply.github.com>
2022-04-23 23:58:05 -0400

    Updated ConstatA.cs Docs

--- ConstantA.cs
+++ ConstantA.cs
@@ -1,19 +1,40 @@
-﻿using System.Collections;
+﻿/*
+ * ConstantA.CS
+ * Original code by Otto Daniels
+ * 
+ * This scirpt was intended to apply a constant force to the
+ * object it was attached to to demonstrate the difference
+ * between the relativistc physics speed cap and the Newtonian
+ * physics lack of cap. 
+ * 
+ * force = force vector to be applied to the object's Rigidbody
+ * on each update.
+ * 
+ * displayDebug = set to true to display debug information such
+ * as the objects velocity, deltaV, and the current mass of its
+ * Rigid body.
+ * 
+ * This script can be applied to any object with a Rigidbody,
+ * regardless of if it has RelativityPhysics or not. If it
+ * doesn't the object will accelerate constantly like in
+ * Newtonian physics. Otherwise, acceleration will slow as
+ * its velocity approaces c like in relativistic physics. 
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstantA : MonoBehaviour
 {
-    
-
     Rigidbody rb;
 
     public Vector3 force = new Vector3();
+    public bool displayDebug;
 
     float v = 0;
     float lastV;
     float deltaV;
-
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +48,16 @@ public class ConstantA : MonoBehaviour
 
         rb.AddForce(force);
 
-        lastV = v;
-        v = rb.velocity.magnitude;
-        deltaV = v - lastV;
+        if (displayDebug)
+        {
+            lastV = v;
+            v = rb.velocity.magnitude;
+            deltaV = v - lastV;
 
-        Debug.Log("Velocity: " + v);
-        Debug.Log("DeltaV:" + deltaV);
-        Debug.Log("Mass: " + rb.mass);
+            Debug.Log("Velocity: " + v);
+            Debug.Log("DeltaV:" + deltaV);
+            Debug.Log("Mass: " + rb.mass);
 
+        }
     }
 }
